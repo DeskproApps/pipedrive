fbf205310a91ba7586d5bee481fc5226a72733b2
OJ Abba <phavorabba@gmail.com>
2025-02-27 14:22:10 +0000

    [Feat] Add logout functionality

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,19 +1,39 @@
-import {
-  TwoButtonGroup,
-  useInitialisedDeskproAppClient,
-} from "@deskpro/app-sdk";
-import { useState } from "react";
-import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { CreateContact } from "../components/CreateContact";
-import { FindContact } from "../components/FindContact";
+import { AppElementPayload, TwoButtonGroup, useDeskproAppEvents, useDeskproElements, useInitialisedDeskproAppClient } from "@deskpro/app-sdk";
 import { Container } from "../components/common";
+import { CreateContact } from "../components/CreateContact";
+import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FindContact } from "../components/FindContact";
+import { useLogout } from "@/api/deskpro";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Contacts = () => {
   const [currentPage, setcurrentPage] = useState("Find Contact");
 
+  const { logoutActiveUser } = useLogout()
+  const navigate = useNavigate();
+
+  useDeskproElements(({ clearElements, registerElement }) => {
+    clearElements();
+    registerElement("pipedriveHomeButton", { type: "home_button" })
+    registerElement("pipedriveRefreshButton", { type: "refresh_button" })
+    registerElement("pipedriveMenuButton", { type: "menu", items: [{ title: "Logout" }] })
+  }, [])
+
+  useDeskproAppEvents({
+    onElementEvent(id: string, _type: string, _payload?: AppElementPayload) {
+      switch (id) {
+        case "pipedriveHomeButton":
+          navigate("/home");
+          break;
+        case "pipedriveMenuButton":
+          logoutActiveUser()
+          break;
+      }
+    },
+  })
+
   useInitialisedDeskproAppClient((client) => {
-    client.deregisterElement("pipedriveEditButton");
-    client.deregisterElement("pipedriveMenuButton");
     client.setTitle(currentPage);
   });
 
