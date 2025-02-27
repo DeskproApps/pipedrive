ddc0303ffaf974ee1306b00402893b174ae5c128
OJ Abba <phavorabba@gmail.com>
2025-02-27 14:43:39 +0000

    [Bug] Fix edit button UI bug

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -35,6 +35,7 @@ export const Contacts = () => {
 
   useInitialisedDeskproAppClient((client) => {
     client.setTitle(currentPage);
+    client.deregisterElement("pipedriveEditButton")
   });
 
   return (
