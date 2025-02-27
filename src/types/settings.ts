7f3e0b7584c8ae183540e94c399849f957f7f107
OJ Abba <phavorabba@gmail.com>
2025-02-27 13:47:11 +0000

    [Feat] Add OAuth2/Global proxy settings

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,4 +1,15 @@
 export interface Settings {
     api_key?: string;
     instance_domain?: string;
+    client_id?: string,
+    use_deskpro_saas?: boolean,
+    use_access_token?: boolean,
+}
+
+export type TicketData = {
+    ticket: {
+        id: string,
+        subject: string,
+        permalinkUrl: string,
+    },
 }
